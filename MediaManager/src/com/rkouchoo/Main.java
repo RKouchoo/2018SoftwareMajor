3f515530998900645a843941cd5082f87fd18e77
kouc01 <kouc01@SP4-0828262646.stpiusx.local>
2018-01-31 15:07:49 +1100

    added simple command verification and testing

--- MediaManager/src/com/rkouchoo/Main.java
+++ MediaManager/src/com/rkouchoo/Main.java
@@ -1,15 +1,22 @@
 package com.rkouchoo;
 
+import java.util.List;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
 	private static BufferedReader reader;
-	private static String lastName;
-	private static String name;
+	private static String lastCommand;
+	private static String command = "";
+	
 	private static Thread readerThread;
+	private static Thread commandHandlerThread;
+	
+	public static final String[] COMMAND_ARRAY = new String[] {"echo", "ping", "hi"};
+	public static final List<String> COMMAND_LIST = Arrays.asList(COMMAND_ARRAY);
 	
 	public static void main(String[] args) throws IOException {
 		
@@ -20,24 +27,47 @@ public class Main {
 		readerThread = new Thread(new Runnable() {
 	         @Override
 	         public void run()  {
-	        	 while (name == lastName) {
+	        	 while (command == lastCommand) {
 	        		 try {
-	        			 System.out.println(read());
+	        			 command = read();
+	        			 lastCommand = command;
 	        		 } catch (IOException ex) {
 	        			 System.out.println(ex);
 	        		 }	
 	     		}	 
 	         }
 	});		
-
+		
+		commandHandlerThread = new Thread(new Runnable() {
+	         @Override
+	         public void run() {
+	        	 try {
+					updateCommandHandler(command);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+	         }
+		});
+		
 		readerThread.start();
+		commandHandlerThread.start();
 		
 	}
 	
 	public static String read() throws IOException {
-		 name = reader.readLine();
-		 lastName = name;
-		 return name;
+		 command = reader.readLine();
+		 return command;
+	}
+
+	public static void updateCommandHandler(String command) throws IOException {
+		
+		if (COMMAND_LIST.contains(command)) {
+			System.out.println("true");
+		} else {
+			 System.out.println("Invalid command: " + command);
+		}
+		
+		
 	}
 	
 	
