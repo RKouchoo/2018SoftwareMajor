ee1d757278503d70708816e8fe3e41a53ad1f347
RKouchoo <r.kouchoo1@gmail.com>
2018-05-10 12:44:10 +1000

    Finished file reader and added comments

--- MediaManagerPilot/src/com/rkouchoo/mm/file/FileCommentReader.java
+++ MediaManagerPilot/src/com/rkouchoo/mm/file/FileCommentReader.java
@@ -1,9 +1,11 @@
 package com.rkouchoo.mm.file;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.rkouchoo.mm.util.FileInformationSupplier;
+import com.rkouchoo.mm.util.MessageUtil;
 
 public class FileCommentReader {
 
@@ -12,41 +14,83 @@ public class FileCommentReader {
 	private List<FileInformationSupplier> informationSuppliers;
 	private String jsonString;
 	
+	private MessageUtil messenger;
 	private JsonFileReader reader;
 	
-	public FileCommentReader() {
-		reader = new JsonFileReader();
+	public FileCommentReader(MessageUtil msg) {
+		this.messenger = msg;
+		this.reader = new JsonFileReader();
 	}
 	
+	/**
+	 * Runs through loads the file to lists
+	 * @param dir  path to hidden file
+	 * @return
+	 */
 	public FileCommentReader runAutomated(File dir) {
-		cleanUp(); // run the cleanup method.
-		this.jsonString = reader.read(dir);
+		cleanUp(); // run the cleanup method, dont need to persistently store info.
+		
+		try {
+			this.jsonString = reader.read(dir);
+		} catch (Throwable t) {
+			messenger.showErrorMessage("failed to read json file", "json error");
+			messenger.showThrowable(t);
+		}
+		
 		this.informationSuppliers = reader.readFromJson(this.jsonString);
-		this.fileKeys = splitSuppliersToKeys(informationSuppliers);
-		this.fileComments = splitSuppliersToComments(informationSuppliers);
+		this.fileKeys = splitSuppliersToKeys(this.informationSuppliers);
+		this.fileComments = splitSuppliersToComments(this.informationSuppliers);
 		
 		return this;
 	}
 	
-
+	/**
+	 * Splits the supplier to a list of file keys
+	 * @param suppliers
+	 * @return
+	 */
 	private List<String> splitSuppliersToKeys(List<FileInformationSupplier> suppliers) {
-		
-		return null;
-	}
-		
-	private List<String> splitSuppliersToComments(List<FileInformationSupplier> suppliers) {
-		
-		return null;
+		List<String> keys = new ArrayList<String>();
+	
+		for (FileInformationSupplier supplier : suppliers) {
+			keys.add(supplier.getUniqueFileID());
+		}
+		return keys;
 	}
 	
+	/**
+	 * Splits the supplier into a list of comments
+	 * @param suppliers
+	 * @return
+	 */
+	private List<String> splitSuppliersToComments(List<FileInformationSupplier> suppliers) {
+		List<String> comments = new ArrayList<String>();
+		
+		for (FileInformationSupplier supplier : suppliers) {
+			comments.add(supplier.getComment());
+		}	
+		return comments;
+	}
+	
+	/**
+	 * List<String> comments
+	 * @return list of comments for the specified directory
+	 */
 	public List<String> getComments() {
 		return fileComments;
 	}
 	
+	/**
+	 * List<String> keys
+	 * @return list of file keys for the specified directory
+	 */
 	public List<String> getFileKeys() {
 		return fileKeys;
 	}
 	
+	/**
+	 * reset the feilds in the object.
+	 */
 	private void cleanUp() { 
 		this.jsonString = null;
 		this.informationSuppliers = null;
--- MediaManagerPilot/src/com/rkouchoo/mm/file/JsonFileReader.java
+++ MediaManagerPilot/src/com/rkouchoo/mm/file/JsonFileReader.java
@@ -1,24 +1,51 @@
 package com.rkouchoo.mm.file;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.List;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.rkouchoo.mm.util.FileInformationSupplier;
 
 public class JsonFileReader {
-
+	
+	Gson gson;
 	
 	public JsonFileReader() {
-		// TODO Auto-generated constructor stub
+		gson = new Gson();
 	}
 	
-	public String read(File path) {
-		
-		return null;
+	/**
+	 * returns a string with json meta
+	 * @param path file to read
+	 * @return string of jsom meta
+	 * @throws Throwable
+	 */
+	public String read(File path) throws Throwable {
+		 BufferedReader br = new BufferedReader(new FileReader(path.getAbsolutePath()));
+		    try {
+		        StringBuilder sb = new StringBuilder();
+		        String line = br.readLine();
+
+		        while (line != null) {
+		            sb.append(line);
+		            sb.append("\n");
+		            line = br.readLine();
+		        }
+		        return sb.toString();
+		    } finally {
+		        br.close();
+		    }
 	}
 	
+	/**
+	 * Converts a json string to a list of FileInformationSuppliers,
+	 * @param jsonString
+	 * @return list of FileInformationSuppliers
+	 */
 	public List<FileInformationSupplier> readFromJson(String jsonString) {
-		
-		return null;
+		return gson.fromJson(jsonString, new TypeToken<List<FileInformationSupplier>>(){}.getType());
 	}
 }
